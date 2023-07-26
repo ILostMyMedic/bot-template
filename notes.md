ddffcea9abee7b585d3255f5e8f2cc80c4ea86c7
ILostMyMedic <martin.johansen.98@outlook.com>
2023-07-26 13:17:39 +0200

    updated notes, checking for env variables.

--- notes.md
+++ notes.md
@@ -1,13 +1,10 @@
 # TODO
 fetch API KEY
-transfer to certification connection to database
-add docs
-add API points
 add events
 middleware
 commands
 plugins
 validations for events
 validations for permissions
-add constructors
+add builders
 assets
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,52 @@
+import dotenv from "dotenv";
+dotenv.config();
 import logger from "./utils/logger";
 import "./utils/processes";
 import environment from "./utils/environment";
 import Database from "./database";
 
-// 
+//
 import Client from "./apps/client";
 import Server from "./apps/express";
 
+const requiredEnvs = ["NODE_ENV", "TOKEN", "PORT", "MONGO_URI"];
+const validateEnv = (envs: string[]): void => {
+	let missingEnvs: boolean = false;
+
+	requiredEnvs.forEach((variable) => {
+		if (!envs.includes(variable)) {
+			missingEnvs = true;
+			logger.error(`Missing environment variable: ${variable}`);
+		} else {
+			const value = process.env[variable];
+			logger.info(`✅ ${variable}`);
+		}
+	});
+
+	if (missingEnvs) {
+		logger.error("Script stopped due to missing environment variables.");
+		process.exit(1);
+	}
+};
+
 const start = async () => {
-    try {
-        await Database.getInstance(environment.mongo).connect();
+	try {
+		// validate environment variables
+		const envVariables = process.env;
+		validateEnv(Object.keys(envVariables));
 
-        // start applications
-        Client.getInstance().connect();
-        const client = Client.getInstance().getClient();
+		// connect to database
+		await Database.getInstance(environment.mongo).connect();
 
-        const server = Server.getInstance(client).connect();
+		// start applications
+		Client.getInstance().connect();
+		const client = Client.getInstance().getClient();
 
-
-    } catch (error) {
-        logger.error(error);
-        process.exit(1);
-    }
+		Server.getInstance(client).connect();
+	} catch (error) {
+		logger.error(error);
+		process.exit(1);
+	}
 };
 
 start();
